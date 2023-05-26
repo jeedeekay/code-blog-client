5d76d682a1384afb1cdcd0c874e59db5626202ee
Gavin Kinder <gavinkinder@gmail.com>
2023-05-26 13:22:40 -0700

    Post Form Uploader, Pre Model Schema

--- src/Pages/CreatePost.js
+++ src/Pages/CreatePost.js
@@ -1,7 +1,71 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image'],
+        ['clean']
+    ]
+}
+
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
+
 const CreatePost = () => {
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+    const [files, setFile] = useState('');
+
+    const createNewPost = async (ev) => {
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data
+        });
+        console.log(await response.json());
+    }
+
     return (
-        <div>Create your post here</div>
-    )
+        <form onSubmit={createNewPost}>
+            <input
+                type='title'
+                placeholder={'Title'}
+                value={title}
+                onChange={ev => setTitle(ev.target.value)}
+            />
+            <input
+                type='summary'
+                placeholder={'Summary'}
+                value={summary}
+                onChange={ev => setSummary(ev.target.value)}    
+            />
+            <input
+                type='file'
+                onChange={ev => setFile(ev.target.files)}
+            />
+            <ReactQuill
+                value={content}
+                modules={modules}
+                formats={formats}
+                onChange={newValue => setContent(newValue)}
+            />
+            <button style={{ marginTop: '5px' }}>Create post</button>
+        </form>
+    );
 }
 
 export default CreatePost;
